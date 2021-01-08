dba5b930fde84d1b4853be9c80b56fa9e6ca63ce
Mark Prins <1165786+mprins@users.noreply.github.com>
2021-01-08 12:23:41 +0100

    Oracle integration testing als GH action

---
+++ .build/ci/oracle-start.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+docker version
+
+# this docker image has the following users/credentials (user/password = system/oracle)
+docker pull pvargacl/oracle-xe-18.4.0:latest
+docker run --rm -p 15210:1521 --name jdbcutil -h jdbcutil -d pvargacl/oracle-xe-18.4.0:latest
+
+# start the dockerized oracle-xe instance
+# this container can be stopped using:
+#
+#    docker stop jdbcutil
+#
+printf "\n\nStarting Oracle XE container, this could take a few minutes..."
+printf "\nWaiting for Oracle XE database to start up.... "
+_WAIT=0;
+while :
+do
+    printf " $_WAIT"
+    if $(docker logs jdbcutil | grep -q 'DATABASE IS READY TO USE!'); then
+        printf "\nOracle XE Database started\n\n"
+        break
+    fi
+    sleep 10
+    _WAIT=$(($_WAIT+10))
+done
+
+# docker ps -a
+# print logs
+docker logs jdbcutil

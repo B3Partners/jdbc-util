54f07319f7dfadac77caa88985afdb33572bf4e5
Mark Prins <1165786+mprins@users.noreply.github.com>
2021-04-08 17:26:06 +0200

    probeer zomertijd probleem met oracle en jenkins te fixen

--- src/test/java/nl/b3p/loader/jdbc/ObjectConverterIntegrationTest.java
+++ src/test/java/nl/b3p/loader/jdbc/ObjectConverterIntegrationTest.java
@@ -1,20 +1,5 @@
 package nl.b3p.loader.jdbc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
-
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.time.Instant;
-import java.util.Calendar;
-import java.util.Locale;
-import java.util.TimeZone;
-import java.util.stream.Stream;
 import nl.b3p.AbstractDatabaseIntegrationTest;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -24,14 +9,24 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.sql.*;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
 public class ObjectConverterIntegrationTest extends AbstractDatabaseIntegrationTest {
     private static final Log LOG = LogFactory.getLog(ObjectConverterIntegrationTest.class);
 
     static Stream<Arguments> dateProvider() {
         return Stream.of(
                 // tabel, kolom, expected epoch millis localtime, input
-                arguments("bericht", "datum", /*GMT+1: 1607063255000L*/ /*GMT*/ 1607066855000L, "2020-12-04T07:27:35"),
-                arguments("job", "datum", /*GMT+1: 1607018340000L*/ /*GMT*/ 1607021940000L, "2020-12-03T18:59:00")
+                arguments("bericht", "datum", /*GMT+1: 1607063255000L*/ /*GMT*/ 1607066855 * 1000L, "2020-12-04T07:27:35"),
+                arguments("bericht", "datum",  /*GMT*/ 1591255655 * 1000L, "2020-06-04T07:27:35"),
+                arguments("job", "datum", /*GMT+1: 1607018340000L*/ /*GMT*/ 1607021940 * 1000L, "2020-12-03T18:59:00")
         );
     }
 
@@ -39,13 +34,6 @@ public class ObjectConverterIntegrationTest extends AbstractDatabaseIntegrationT
         return Stream.of(
                 // tabel, kolom, input
                 arguments("bericht", "soort", "input"),
-                // CLOB kolom in Oracle, niet ondersteund
-                //arguments("job", "br_xml",
-                //        "And that's when it becomes fun - you don't have to spend your time thinking about what's " +
-                //                "happening - you just let it happen. Let's give him a friend too. Everybody needs a " +
-                //                "friend. You gotta think like a tree. That's what makes life fun. That you can make " +
-                //                "these decisions. That you can create the world that you want. Fluff that up. " +
-                //                "Everything is happy if you choose to make it that way.")
                 arguments("bericht", "object_ref",
                         "And that's when it becomes fun - you don't have to spend your time thinking about what's " +
                                 "happening - you just let it happen. Let's give him a friend too. Everybody needs a " +
@@ -77,16 +65,29 @@ public class ObjectConverterIntegrationTest extends AbstractDatabaseIntegrationT
                     tableName,
                     colName
             );
+
             assertTrue(o instanceof java.sql.Date);
+
+            LOG.debug("converted datum object (input: "+input+"): " + (new java.text.SimpleDateFormat()).format (((Date)o)));
+
             Calendar cal = Calendar.getInstance();
             cal.setTimeZone(TimeZone.getTimeZone("GMT"));
             cal.setTimeInMillis(expected);
             cal.setTimeZone(TimeZone.getDefault());
 
+            long utcOffset = cal.getTimeZone().getOffset(expected);
+
+            LOG.debug(
+                    "calendar timezone '" + cal.getTimeZone().getDisplayName()
+                            + "', is DST ?: " + cal.getTimeZone().inDaylightTime(new Date(expected))
+                            + ", DST offset : " + cal.getTimeZone().getDSTSavings()
+                            + ", UTC offset : " + utcOffset
+            );
+
             assertEquals(
-                    cal.getTime().getTime()-cal.getTimeZone().getOffset(Instant.now().toEpochMilli()),
+                    cal.getTime().getTime() - utcOffset,
                     ((Date) o).getTime(),
-                    "datum is ongelijk, kan een tijdzone verschil zijn"
+                    "datum is ongelijk, dat kan een tijdzone/zomertijd verschil zijn"
             );
 
         } catch (SQLException e) {
@@ -136,7 +137,7 @@ public class ObjectConverterIntegrationTest extends AbstractDatabaseIntegrationT
                 columnMetadata = new ColumnMetadata(columnsRs);
             }
         }
-        LOG.debug("metadata voor: " + colName + " is: " + columnMetadata);
+        LOG.debug("metadata voor kolom: " + colName + " is: " + columnMetadata);
         return columnMetadata;
     }
 

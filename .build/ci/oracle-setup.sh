228836122cc0bc1aa701ee2e048b99e94ba07981
Mark Prins <1165786+mprins@users.noreply.github.com>
2024-01-05 17:38:02 +0100

    [BRMO-343] Oracle 23c opnemen in test matrix
    using service name instead of SID

--- .build/ci/oracle-setup.sh
+++ .build/ci/oracle-setup.sh
@@ -2,5 +2,5 @@
 printf "\nSetup database...\n"
 
 # note port number is the port inside the container!
-docker exec -i jdbcutil sqlplus -l system/oracle@//localhost:1521/XE < ./.build/ci/oracle-create-user.sql
-docker exec -i jdbcutil sqlplus -l jdbcutil/jdbcutil@//localhost:1521/XE < ./.build/ci/oracle-create-schema.sql
+docker exec -i jdbcutil sqlplus -l system/oracle@//localhost:1521/FREE < ./.build/ci/oracle-create-user.sql
+docker exec -i jdbcutil sqlplus -l jdbcutil/jdbcutil@//localhost:1521/FREE < ./.build/ci/oracle-create-schema.sql

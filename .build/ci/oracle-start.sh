94fd1fb4b13118ddf176a9281ba20ccbef5f26a2
Mark Prins <1165786+mprins@users.noreply.github.com>
2024-01-08 21:01:56 +0100

    [BRMO-343] Oracle 23c opnemen in test matrix (#541)
    [BRMO-343] Oracle 23c opnemen in test matrix
    using service name instead of SID

--- .build/ci/oracle-start.sh
+++ .build/ci/oracle-start.sh
@@ -2,22 +2,22 @@
 docker version
 
 # this docker image has the following users/credentials (user/password = system/oracle)
-docker pull ghcr.io/gvenzl/oracle-xe:$1
-docker run --rm -p 15210:1521 -e ORACLE_PASSWORD=oracle --name jdbcutil -h jdbcutil -d ghcr.io/gvenzl/oracle-xe:$1
+docker pull ghcr.io/gvenzl/$1
+docker run --rm -p 15210:1521 -e ORACLE_PASSWORD=oracle --name jdbcutil -h jdbcutil -d ghcr.io/gvenzl/$1
 
 # start the dockerized oracle-xe instance
 # this container can be stopped using:
 #
 #    docker stop jdbcutil
 #
-printf "\n\nStarting Oracle $1 XE container, this could take a few minutes..."
-printf "\nWaiting for Oracle $1 XE database to start up.... "
+printf "\n\nStarting Oracle $1 ontainer, this could take a few minutes..."
+printf "\nWaiting for Oracle $1 database to start up.... "
 _WAIT=0;
 while :
 do
     printf " $_WAIT"
     if $(docker logs jdbcutil | grep -q 'DATABASE IS READY TO USE!'); then
-        printf "\nOracle $1 XE Database started\n\n"
+        printf "\nOracle $1 Database started\n\n"
         break
     fi
     sleep 10
--- src/main/java/nl/b3p/jdbc/util/converter/OracleJdbcConverter.java
+++ src/main/java/nl/b3p/jdbc/util/converter/OracleJdbcConverter.java
@@ -82,7 +82,7 @@ public class OracleJdbcConverter extends GeometryJdbcConverter {
 
   @Override
   public String getGeomTypeName() {
-    return "SDO_GEOMETRY";
+    return "MDSYS.SDO_GEOMETRY";
   }
 
   /*

2796b38bb935054123568939f7c50c954ca231b1
Mark Prins <1165786+mprins@users.noreply.github.com>
2024-02-06 10:30:34 +0100

    Oracle CI updates

--- .build/ci/oracle-setup.sh
+++ .build/ci/oracle-setup.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
+set -e
+export SQLPATH=./.build/ci
 printf "\nSetup database...\n"
+
+
+docker exec -i jdbcutil sqlplus -l sys/oracle@//localhost:1521/FREE as sysdba < ./.build/ci/oracle-system-setup.sql
+docker exec -i jdbcutil sqlplus -s /nolog<<EOF
+conn sys/oracle as sysdba
+startup
+exit
+EOF
 
 # note port number is the port inside the container!
 docker exec -i jdbcutil sqlplus -l system/oracle@//localhost:1521/FREE < ./.build/ci/oracle-create-user.sql

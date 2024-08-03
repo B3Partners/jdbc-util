f5bf335c091ae2847177b660d5f09e262f5aad30
Mark Prins <1165786+mprins@users.noreply.github.com>
2024-08-03 13:24:49 +0200

    SQL Server workflow fixes
    to work around th echange in path and behaviour for the `-latest` versions of mssql
    
    see https://github.com/microsoft/mssql-docker/issues/892

--- .build/ci/mssql-setup.sh
+++ .build/ci/mssql-setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 printf "\nSetup database...\n"
-docker exec -i jdbcutil /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "Password12!" -d "master" -Q 'CREATE DATABASE staging'
+docker exec -i jdbcutil /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P "Password12!" -No -d "master" -Q 'CREATE DATABASE staging'
 docker cp ./.build/ci/mssql-create-schema.sql jdbcutil:/home/
-docker exec -i jdbcutil /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "Password12!" -d "staging" -i /home/mssql-create-schema.sql
+docker exec -i jdbcutil /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P "Password12!" -No -d "staging" -i /home/mssql-create-schema.sql
